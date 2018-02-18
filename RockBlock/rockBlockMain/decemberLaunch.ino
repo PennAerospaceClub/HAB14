b0a29ca8067aea6dbcff67eec32b7615c9d014ad
khallor1 <kieran_halloran@yahoo.com>
2018-02-18 12:36:22 -0500

    updated december_launch to recieve data via softwareSerial

--- RockBlock/rockBlockMain/decemberLaunch.ino
+++ RockBlock/rockBlockMain/decemberLaunch.ino
@@ -15,6 +15,10 @@ IridiumSBD isbd(nss, sleepPin);
 uint8_t buffer[200];
 
 
+SoftwareSerial inc(5, 6); //for incoming data (rx is pin 5, tx is pin 6)
+String Data = ""; //to store incoming data
+
+
 int messagesSent  = 0;
 size_t bufferSize = 0;
 
@@ -35,6 +39,9 @@ void setup()
   isbd.setPowerProfile(0);
   isbd.begin();
 
+  //begin communication with other arduino
+  inc.begin(9600);
+
   //take a look at the code below for reference of checking signal quality
   int err = isbd.getSignalQuality(signalQuality);
   if (err != 0)
@@ -53,43 +60,51 @@ void setup()
 
 void repeatMe() //would like to eventually implement the Message class
 {
-  
- Serial.print("hello!"); 
- char outBuffer[200]; //RockBlock works in terms of char buffers
+  while (inc.available())
+  {
+    char character = mySerial.read(); // Receive a single character from the software serial port
+    Data.concat(character); // Add the received character to the receive buffer
 
- String messageToSend = "Hello World (new!)"; //Sample string to send, will update to 
- //interface with other teams
- messagesSent += 1; 
-for(uint8_t i = 0; i < messageToSend.length(); i++){
-  
-  outBuffer[i] = messageToSend[i]; 
-}
+  }
+  String messageToSend = Data; //Sample string to send, will update to
+  Data = "";
 
-uint8_t rxBuffer[200] = {0};
-for(int i = 0; i < messageToSend.length(); i++){
-      rxBuffer[i] = outBuffer[i];
-}
+  Serial.print("hello!");
+  char outBuffer[200]; //RockBlock works in terms of char buffers
 
-bufferSize = sizeof(buffer);
 
-isbd.sendReceiveSBDBinary(rxBuffer, messageToSend.length(), buffer, bufferSize); 
- 
- for (int i = 0; i < sizeof(buffer); ++i)
-    {
-      Serial.write(buffer[i]); 
-     // Serial.print("(");
-     // Serial.print(bufferIn[i], HEX);
-     // Serial.print(") in");
-    }
+  //interface with other teams
+  messagesSent += 1;
+  for (uint8_t i = 0; i < messageToSend.length(); i++) {
+
+    outBuffer[i] = messageToSend[i];
+  }
+
+  uint8_t rxBuffer[200] = {0};
+  for (int i = 0; i < messageToSend.length(); i++) {
+    rxBuffer[i] = outBuffer[i];
+  }
+
+  bufferSize = sizeof(buffer);
+
+  isbd.sendReceiveSBDBinary(rxBuffer, messageToSend.length(), buffer, bufferSize);
+
+  for (int i = 0; i < sizeof(buffer); ++i)
+  {
+    Serial.write(buffer[i]);
+    // Serial.print("(");
+    // Serial.print(bufferIn[i], HEX);
+    // Serial.print(") in");
+  }
 
 }
 
 
 void loop()
 {
-timer.run(); 
+  timer.run();
 
-  
+
 }
 
 
